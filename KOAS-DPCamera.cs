a83ace38bb414bcfd5d601c8fa97456e451dbf60
unknown-user <unknown-user@fe80::31b5:d04b:5067:f0a9%4>
2016-04-19 10:35:22 -0700

    Made button states global; improved variable names

--- KOAS-DPCamera.cs
+++ KOAS-DPCamera.cs
@@ -16,22 +16,20 @@ using System;
 using System.IO;
 using UnityEngine;
 
-namespace DPCamera
-{
-	public class DPCamera : PartModule
-	{
+namespace DPCamera {
+	public class DPCamera : PartModule {
 		[KSPField]
 		public Vector3
 			cameraPosition = Vector3.zero;		//Eyepoint
 		[KSPField]
 		public Vector3
-			cameraForward = Vector3.forward;	//Viewing Direction
+			cameraForward = Vector3.forward;	//Viewing TOWARD Direction
 		[KSPField]
 		public Vector3
-			cameraUp = Vector3.up;			//UP direction
+			cameraUp = Vector3.up;			//Viewing UP Direction
 		[KSPField]
 		public string
-			cameraTransformName = "";		//transform
+			cameraTransformName = "";		//transform 
 		[KSPField]
 		public float
 			cameraFoV = 60;				//Zoom (FOV angle)
@@ -41,36 +39,37 @@ namespace DPCamera
 		[KSPField(isPersistant = false)]
 		public string
 			cameraName = "DPCam";			//Name Ident
-		public FlightCamera stdCam;
-		protected static Transform sOrigParent;
-		protected static Quaternion sOrigRotation = Quaternion.identity;
-		protected static Vector3 sOrigPosition = Vector3.zero;
-		protected static float sOrigFov;
-		protected static float sOrigClip;
+		public FlightCamera nativeCam;
+		protected static Transform storedCamParent;
+		protected static Quaternion storedCamRotation = Quaternion.identity;
+		protected static Vector3 storedCamPosition = Vector3.zero;
+		protected static float storedCamFoV;
+		protected static float storedCamClipPlane;
 		bool inDPCam;
 		bool unlockReady;
 		bool mapWasActive;
+		bool buttonIsDeactivate;
 		public Texture2D crosshairTexture;
 		public float crosshairScale = 1;
+
 		/*
 	* This event is active when controlling the vessel with the part. 
 	* Adds "Clicklable Button" to rightclick GUI. When the new button is
 	* clicked, the ActivateEvent() function is called.
 	*/
 		[KSPEvent(guiActive = true, guiName = "View from Here")]
-		public void ActivateEvent ()
-		{
+		public void ActivateEvent () {
 			if (!inDPCam) {
-				ScreenMessages.PostScreenMessage ("Docking View Activated - Press " + GameSettings.CAMERA_MODE.primary + " to Escape", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+				ScreenMessages.PostScreenMessage ("Docking View Activated - Press " + GameSettings.CAMERA_MODE.primary + " to Exit", 5.0f, ScreenMessageStyle.UPPER_CENTER);
 				
-				stdCam = FlightCamera.fetch;
+				nativeCam = FlightCamera.fetch;
 				
 				// Grab all Standard ingame camera parameters
-				sOrigParent = stdCam.transform.parent;
-				sOrigClip = Camera.main.nearClipPlane;
-				sOrigFov = Camera.main.fieldOfView;
-				sOrigPosition = stdCam.transform.localPosition;
-				sOrigRotation = stdCam.transform.localRotation;
+				storedCamParent = nativeCam.transform.parent;
+				storedCamClipPlane = Camera.main.nearClipPlane;
+				storedCamFoV = Camera.main.fieldOfView;
+				storedCamPosition = nativeCam.transform.localPosition;
+				storedCamRotation = nativeCam.transform.localRotation;
 				
 				// Set parameters to new values
 				setDPCam ();
@@ -82,8 +81,7 @@ namespace DPCamera
 				// Flag for FixedUpdate conditional statement below
 				inDPCam = true;
 				// This will hide the Activate event, and show the Deactivate event.
-				Events ["ActivateEvent"].active = false;
-				Events ["DeactivateEvent"].active = true;
+				buttonIsDeactivate = true;
 			} else {
 				Debug.LogWarning ("[DPCamera] DPCam Activated when while inDPCam");
 			}
@@ -93,37 +91,33 @@ namespace DPCamera
 	 */
 		
 		[KSPEvent(guiActive = true, guiName = "Close this Window", active = false)]
-		public void DeactivateEvent ()
-		{
+		public void DeactivateEvent () {
 			// Remove the rightclick GUI so it's not in the way
 			UIPartActionController.Instance.Deselect (true);
 		}
 		
-		public void setDPCam ()
-		{
-			stdCam.setTarget (null);
-			stdCam.transform.parent = (cameraTransformName.Length > 0) ? part.FindModelTransform (cameraTransformName) : part.transform;
+		public void setDPCam ()	{
+			nativeCam.setTarget (null);
+			nativeCam.transform.parent = (cameraTransformName.Length > 0) ? part.FindModelTransform (cameraTransformName) : part.transform;
 			Camera.main.nearClipPlane = cameraClip;
-			stdCam.SetFoV (cameraFoV);
-			stdCam.transform.localPosition = cameraPosition;
-			stdCam.transform.localRotation = Quaternion.LookRotation (cameraForward, cameraUp);
+			nativeCam.SetFoV (cameraFoV);
+			nativeCam.transform.localPosition = cameraPosition;
+			nativeCam.transform.localRotation = Quaternion.LookRotation (cameraForward, cameraUp);
 		}
 
-		public void unsetDPCam ()
-		{
+		public void unsetDPCam () {
 			// Set parameters to old values
-			stdCam.transform.parent = sOrigParent;
-			stdCam.transform.localPosition = sOrigPosition;
-			stdCam.transform.localRotation = sOrigRotation;
-			Camera.main.nearClipPlane = sOrigClip;
-			stdCam.SetFoV (sOrigFov);
+			nativeCam.transform.parent = storedCamParent;
+			nativeCam.transform.localPosition = storedCamPosition;
+			nativeCam.transform.localRotation = storedCamRotation;
+			Camera.main.nearClipPlane = storedCamClipPlane;
+			nativeCam.SetFoV (storedCamFoV);
 			if (FlightGlobals.ActiveVessel != null && HighLogic.LoadedScene == GameScenes.FLIGHT) {
-				stdCam.setTarget (FlightGlobals.ActiveVessel.transform);
+				nativeCam.setTarget (FlightGlobals.ActiveVessel.transform);
 			}
 			
 			// This will hide the Deactivate event, and show the Activate event.
-			Events ["ActivateEvent"].active = true;
-			Events ["DeactivateEvent"].active = false;
+			buttonIsDeactivate = false;
 			
 			// Flag for FixedUpdate conditional statement, keeps this block from executing
 			inDPCam = false;
@@ -138,12 +132,11 @@ namespace DPCamera
 		
 		public void FixedUpdate ()
 		{
-			if (inDPCam)
-			{
+			if (inDPCam) {
 				if (!MapView.MapIsEnabled && (Input.GetKeyDown (GameSettings.CAMERA_MODE.primary) || GameSettings.FOCUS_NEXT_VESSEL.GetKeyDown () || GameSettings.FOCUS_PREV_VESSEL.GetKeyDown ())) {
 					unsetDPCam ();		
 				}
-				if (stdCam.Target != null) {
+				if (nativeCam.Target != null) {
 					unsetDPCam ();
 					unlockReady = true;
 				}
@@ -154,25 +147,27 @@ namespace DPCamera
 					setDPCam ();
 					mapWasActive = false;
 				}
-			}
-			else
-			{
-				if (Input.GetKeyUp (GameSettings.CAMERA_MODE.primary) && InputLockManager.IsLocked (ControlTypes.CAMERAMODES))
-		    		{
+			} else {
+				if (Input.GetKeyUp (GameSettings.CAMERA_MODE.primary) && InputLockManager.IsLocked (ControlTypes.CAMERAMODES)) {
 					unlockReady = true;
 				}
 			}
-			if (unlockReady)
-			{
+			if (unlockReady) {
 				InputLockManager.RemoveControlLock ("DPCamLock");
 				unlockReady = false;
+			}
+			if (buttonIsDeactivate) {
+				Events ["ActivateEvent"].active = false;
+				Events ["DeactivateEvent"].active = true;
+			} else {
+				Events ["ActivateEvent"].active = true;
+				Events ["DeactivateEvent"].active = false;
 			}
 		}
 		
 		/* Adds Recticle to view 
 	*/
-		void OnGUI ()
-		{
+		void OnGUI () {
 			//if not paused and in DPCamera view
 			if (Time.timeScale != 0 && inDPCam && !MapView.MapIsEnabled) {
 				if (crosshairTexture != null)
@@ -184,18 +179,15 @@ namespace DPCamera
 		
 		/* Initialize Routine - Load Texture
 	*/
-		public override void OnStart (StartState state)
-		{
+		public override void OnStart (StartState state)	{
 			
 			crosshairTexture = LoadTextureFile ("reticle.png");
-			
 			base.OnStart (state);
 		}
 		
 		/* Stolen from MovieTime plugin :)
 	*/
-		public static Texture2D LoadTextureFile (string fileName)
-		{
+		public static Texture2D LoadTextureFile (string fileName) {
 			try {
 				string path;
 				
